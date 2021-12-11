9d3e35217d30b4fec9b352be7191047c687bb791
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-12-11 16:24:20 +0900

    title scene

--- public/js/title_scene.js
+++ public/js/title_scene.js
@@ -8,8 +8,17 @@
 const titleScene = new PIXI.Container();
 
 
+// 背景画像の追加
+let backgroundImage = new PIXI.Sprite(new PIXI.Texture.from("images/title.png"));
+backgroundImage.width = gameWindowWidth;
+backgroundImage.height = gameWindowHeight;
+backgroundImage.x = 0;
+backgroundImage.y = 0;
+titleScene.addChild(backgroundImage);
+
+
 // タイトルの配置
-let titleText = new PIXI.Text("タイトル");
+let titleText = new PIXI.Text("The Bartender", {fill: 0xFFFFFF,  align :'center'});
 titleText.style.fontSize = gameWindowWidth/10;
 titleText.x = gameWindowWidth/2 - titleText.width/2;
 titleText.y = gameWindowHeight * 0.2;
@@ -17,12 +26,12 @@ titleScene.addChild(titleText);
 
 
 // ゲームをプレイボタンの配置
-let playButton = new PIXI.Text("ゲームをプレイ");
+let playButton = new PIXI.Text("ゲームをプレイする", {fill: 0xFFFFFF,  align :'center'});
 playButton.interactive = true;
 playButton.buttonMode = true;
-playButton.style.fontSize = gameWindowWidth/30;
+playButton.style.fontSize = gameWindowWidth/20;
 playButton.x = gameWindowWidth/2 - playButton.width/2;
-playButton.y = gameWindowHeight/1.35;
+playButton.y = gameWindowHeight/1.45;
 playButton.on('pointertap', fromTitleSceneToGameScene);
 titleScene.addChild(playButton);
 
