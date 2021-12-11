3602f2449b58dac4a71dbb1e4ea997ca06713cbe
Hoshi Iju <e195713@ie.u-ryukyu.ac.jp>
2021-12-11 15:43:24 +0900

    set images of timer, shelf and glass

--- public/js/game_scene.js
+++ public/js/game_scene.js
@@ -9,7 +9,7 @@
 
 const gameScene = new PIXI.Container();
 
-
+/*
 //セレクトボタン1の配置
 let selectButtonImage1 = new PIXI.Sprite(new PIXI.Texture.from("images/selectButton.png"));
 selectButtonImage1.interactive = true;
@@ -20,4 +20,28 @@ selectButtonImage1.x = 100;
 selectButtonImage1.y = 100;
 selectButtonImage1.on('pointertap', select);
 gameScene.addChild(selectButtonImage1);
+*/
 
+//棚の配置
+let shelfImage = new PIXI.Sprite(new PIXI.Texture.from("./images/shelf.png"));
+shelfImage.width = gameWindowWidth * 0.4;
+shelfImage.height = gameWindowHeight * 0.6;
+shelfImage.x = gameWindowWidth * 0.01;
+shelfImage.y = gameWindowHeight * 0.38;
+gameScene.addChild(shelfImage);
+
+//グラスの配置
+let glassImage = new PIXI.Sprite(new PIXI.Texture.from("./images/glass.png"));
+glassImage.width = gameWindowWidth /5;
+glassImage.height = gameWindowHeight /3;
+glassImage.x = shelfImage.width * 1.2;
+glassImage.y = shelfImage.y + shelfImage.height * 0.4;
+gameScene.addChild(glassImage);
+
+//タイマーの配置
+let timerImage = new PIXI.Sprite(new PIXI.Texture.from("./images/timer.png"));
+timerImage.width = gameWindowWidth /7;
+timerImage.height = timerImage.width;
+timerImage.x = gameWindowWidth * 0.01;
+timerImage.y = gameWindowHeight * 0.01;
+gameScene.addChild(timerImage);
