6d1f668338f522c5c895ab666984b4c1bbe830ee
ひじ方　敦也 <e195749@ie.u-ryukyu.ac.jp>
2021-12-11 18:39:30 +0900

    judge

--- public/js/game_model.js
+++ public/js/game_model.js
@@ -17,3 +17,31 @@ function select(){
 function clickLiqueur(e){
     console.log(e.data.name)
 }
+
+
+// 例
+let myGrass = [1,1,2,2]
+
+let guestGrass1 = [2,2,1,1]
+let guestGrass2 = [1,2,1,2]
+let guestGrass3 = [2,1,2,1]
+let guestGrass4 = [1,1,2,3]
+
+
+
+// 正解判定
+function judge(guestGrass){
+    myGrass.sort() //sort
+    guestGrass.sort() //sort
+    if(JSON.stringify(myGrass) == JSON.stringify(guestGrass)){
+        return true
+    }else{
+        return false
+    }
+}
+
+// 例
+judge(guestGrass1)
+judge(guestGrass2)
+judge(guestGrass3)
+judge(guestGrass4)
