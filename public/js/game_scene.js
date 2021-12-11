9661b80fa04be71e5be3786410a98228bda68842
ひじ方　敦也 <e195749@ie.u-ryukyu.ac.jp>
2021-12-11 21:45:04 +0900

    guest

--- public/js/game_scene.js
+++ public/js/game_scene.js
@@ -46,25 +46,74 @@ gameScene.addChild(bartenderImage);
 // お客さんの枠の表示
 let windowImage1 = new PIXI.Sprite(new PIXI.Texture.from("./images/window.png"));
 windowImage1.width = gameWindowWidth * 0.25;
-windowImage1.height = gameWindowHeight * 0.25;
+windowImage1.height = gameWindowHeight * 0.3;
 windowImage1.x = gameWindowWidth * 0.175;
 windowImage1.y = gameWindowHeight * 0.05;
 gameScene.addChild(windowImage1);
 
+
 let windowImage2 = new PIXI.Sprite(new PIXI.Texture.from("./images/window.png"));
 windowImage2.width = gameWindowWidth * 0.25;
-windowImage2.height = gameWindowHeight * 0.25;
+windowImage2.height = gameWindowHeight * 0.3;
 windowImage2.x = gameWindowWidth * 0.45;
 windowImage2.y = gameWindowHeight * 0.05;
 gameScene.addChild(windowImage2);
 
 let windowImage3 = new PIXI.Sprite(new PIXI.Texture.from("./images/window.png"));
 windowImage3.width = gameWindowWidth * 0.25;
-windowImage3.height = gameWindowHeight * 0.25;
+windowImage3.height = gameWindowHeight * 0.3;
 windowImage3.x = gameWindowWidth * 0.725;
 windowImage3.y = gameWindowHeight * 0.05;
 gameScene.addChild(windowImage3);
 
+
+//お客さんの表示
+let guestImage1 = new PIXI.Sprite(new PIXI.Texture.from("./images/guest1_1.png"));
+guestImage1.width = gameWindowWidth * 0.15;
+guestImage1.height = gameWindowHeight * 0.27;
+guestImage1.x = gameWindowWidth * 0.175;
+guestImage1.y = gameWindowHeight * 0.05;
+gameScene.addChild(guestImage1);
+
+let guestImage2 = new PIXI.Sprite(new PIXI.Texture.from("./images/guest1_2.png"));
+guestImage2.width = gameWindowWidth * 0.15;
+guestImage2.height = gameWindowHeight * 0.27;
+guestImage2.x = gameWindowWidth * 0.45;
+guestImage2.y = gameWindowHeight * 0.05;
+gameScene.addChild(guestImage2);
+
+let guestImage3 = new PIXI.Sprite(new PIXI.Texture.from("./images/guest1_3.png"));
+guestImage3.width = gameWindowWidth * 0.15;
+guestImage3.height = gameWindowHeight * 0.27;
+guestImage3.x = gameWindowWidth * 0.725;
+guestImage3.y = gameWindowHeight * 0.05;
+gameScene.addChild(guestImage3);
+
+
+//グラスの表示
+let guestGlassImage1 = new PIXI.Sprite(new PIXI.Texture.from("./images/glass.png"));
+guestGlassImage1.width = gameWindowWidth * 0.08;
+guestGlassImage1.height = gameWindowHeight * 0.165;
+guestGlassImage1.x = windowImage1.x + guestImage1.width;
+guestGlassImage1.y = gameWindowHeight * 0.08;
+gameScene.addChild(guestGlassImage1);
+
+let guestGlassImage2 = new PIXI.Sprite(new PIXI.Texture.from("./images/glass.png"));
+guestGlassImage2.width = gameWindowWidth * 0.08;
+guestGlassImage2.height = gameWindowHeight * 0.165;
+guestGlassImage2.x = windowImage2.x + guestImage2.width;
+guestGlassImage2.y = gameWindowHeight * 0.08;
+gameScene.addChild(guestGlassImage2);
+
+let guestGlassImage3 = new PIXI.Sprite(new PIXI.Texture.from("./images/glass.png"));
+guestGlassImage3.width = gameWindowWidth * 0.08;
+guestGlassImage3.height = gameWindowHeight * 0.165;
+guestGlassImage3.x = windowImage3.x + guestImage3.width;
+guestGlassImage3.y = gameWindowHeight * 0.08;
+gameScene.addChild(guestGlassImage3);
+
+
+//制限時間の表示
 
 //お酒の配置
 let liqueurNameList = ["passoa", "orange", "lemon", "lime", "gin", "soda", "whiskey", "tonic", "vodka", "u-ron", "cassis", "peach"]
